5058f08faca78410dfdf9640056c1906f75ad219
Aradhya Ganiga <aradhyaganiga@gmail.com>
2025-10-01 09:36:59 +0530

    Time: 104 ms (10.39%), Space: 21.3 MB (44.36%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        #Here we should apply kadane's algo
+        max_sum=current_sum=nums[0]
+        for i in range(1,len(nums)):
+            current_sum=max(current_sum+nums[i],nums[i])
+            max_sum=max(max_sum,current_sum)
+        return max_sum
+
+        
