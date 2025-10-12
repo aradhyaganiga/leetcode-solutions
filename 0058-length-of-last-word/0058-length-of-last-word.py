ca94402c64399521ca36e91d2b914d12709af062
Aradhya Ganiga <aradhyaganiga@gmail.com>
2025-10-13 00:11:49 +0530

    Time: 0 ms (100%), Space: 12.6 MB (45.5%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        s=s.strip()
+        words=s.split(' ')
+        return len(words[-1])
+
+        
+        
+
+
+        
+       
+
+        
